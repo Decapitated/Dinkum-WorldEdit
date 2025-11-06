68838c1663e7b96ebd0b971c985cf4ae9ec2173b
Brett Johnson <brettzkyjo10@gmail.com>
2025-11-06 17:49:13 -0600

    Basic reimplementation of basic tape measure

--- Core.cs
+++ Core.cs
@@ -9,14 +9,27 @@ using Object = UnityEngine.Object;
 
 namespace WorldEditMod
 {
+    using Squares = Dictionary<Vector2Int, TapeMeasureSquare>;
     public class Core : MelonMod
     {
+        static public Core Instance { get; private set; }
+
         static public GameObject SquarePrefab { get; private set; }
+        static public MaterialPropertyBlock MPB { get; private set; }
+
         public override void OnInitializeMelon()
         {
             LoggerInstance.Msg("Initializing...");
+            Instance = this;
             DivineDinkum.Core.Instance.OnSceneReady.Subscribe(OnSceneReady);
-            DivineDinkum.Core.Instance.OnSceneUnready.Subscribe(OnSceneUnready);
+        }
+
+        public override void OnUpdate()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                ClearMeasurement();
+            }
         }
 
         private void OnSceneReady()
@@ -24,24 +37,20 @@ namespace WorldEditMod
             MelonCoroutines.Start(Setup());
         }
 
-        private void OnSceneUnready()
-        {
-
-        }
-
         private IEnumerator Setup()
         {
-            yield return SetupMenuScreen();
-            yield return GetMeasurmentSquarePrefab();
+            SetupMenuScreen();
+            GetMeasurmentSquarePrefab();
+            yield break;
         }
 
-        private IEnumerator SetupMenuScreen()
+        private void SetupMenuScreen()
         {
             Transform quitTransform = DivineDinkum.MapCanvas.Instance.MenuButtons.transform.Find("Quit To Desktop");
             if (quitTransform == null)
             {
                 LoggerInstance.Error("Failed to find Quit button.");
-                yield break;
+                return;
             }
             //yield return new WaitUntil(() => quitTransform.gameObject.activeInHierarchy);
 
@@ -49,25 +58,150 @@ namespace WorldEditMod
             if (quitText == null)
             {
                 LoggerInstance.Error("Failed to find Text.");;
-                yield break;
+                return;
             }
             
             TextMeshProUGUI quitTMP = quitText.GetComponent<TextMeshProUGUI>();
             if (quitTMP == null)
             {
                 LoggerInstance.Error("Failed to get TextMeshProUGUI component.");
-                yield break;
+                return;
             }
 
             quitTMP.SetText("Kill Game");
         }
     
-        private IEnumerator GetMeasurmentSquarePrefab()
+        private void GetMeasurmentSquarePrefab()
         {
             var squarePrefab = DivineDinkum.Utilities.FindResourceObject<GameObject>("MeasurementSquare");
-            //squarePrefab.transform.detach
             SquarePrefab = GameObject.Instantiate(squarePrefab);
-            yield break;
+            GameObject.Destroy(squarePrefab.transform.Find("Text (1)"));
+            GameObject.Destroy(squarePrefab.transform.Find("Text (2)"));
+        }
+
+        enum Mode
+        {
+            Test
+        }
+
+        Mode mode = Mode.Test;
+        Vector3? startPosition = null;
+        Vector3? endPosition = null;
+        Squares squares = new();
+
+        public bool IsMeasuring
+        {
+            get => startPosition != null;
+        }
+
+        internal void UseCustomTapeMeasure()
+        {
+            var highlightPos = DivineDinkum.Utilities.GetHighlighterPosition();
+            LoggerInstance.Msg(
+                "\n Highlight \n" +
+                "===========\n" +
+                $"Pos: {highlightPos}");
+
+            if (startPosition == null)
+            {
+                StartMeasuring(highlightPos);
+            }
+            else if (endPosition == null && highlightPos != startPosition)
+            {
+                StopMeasuring(highlightPos);
+            }
+            else
+            {
+                ClearMeasurement();
+            }
+        }
+
+        void StartMeasuring(Vector3 highlightPos)
+        {
+            LoggerInstance.Msg("Start measuring...");
+            startPosition = highlightPos;
+            endPosition = null;
+            MelonCoroutines.Start(DoMeasurement());
+        }
+
+        void StopMeasuring(Vector3 highlightPos)
+        {
+            LoggerInstance.Msg("Stop measuring...");
+            endPosition = highlightPos;
+        }
+
+        void ClearMeasurement()
+        {
+            LoggerInstance.Msg("Clear measurement...");
+            startPosition = null;
+            endPosition = null;
+            ClearSquares();
+        }
+        
+        void ClearSquares()
+        {
+            foreach (var square in squares)
+            {
+                GameObject.Destroy(square.Value.gameObject);
+            }
+            squares.Clear();
+        }
+
+        IEnumerator DoMeasurement()
+        {
+            LoggerInstance.Msg("Do measurement...");
+            while (startPosition != null)
+            {
+                Vector3 realEndPos = endPosition ?? DivineDinkum.Utilities.GetHighlighterPosition();
+                Squares newSquares = new();
+                switch (mode)
+                {
+                    case Mode.Test:
+                        newSquares = DoTest((Vector3)startPosition, realEndPos);
+                        break;
+                }
+                foreach (var square in squares)
+                {
+                    GameObject.Destroy(square.Value.gameObject);
+                }
+                squares = newSquares;
+                yield return null;
+            }
+        }
+
+        Squares DoTest(Vector3 startPos, Vector3 endPos)
+        {
+            Squares newSquares = new();
+            Vector3 diff = endPos - startPos;
+            int xDir = (int)Mathf.Sign(diff.x);
+            int zDir = (int)Mathf.Sign(diff.z);
+            for (int z = 0; z <= Mathf.Abs(diff.z); z++)
+            {
+                for (int x = 0; x <= Mathf.Abs(diff.x); x++)
+                {
+                    var currentTile = new Vector2Int(
+                        (int)(startPos.x + x * xDir),
+                        (int)(startPos.z + z * zDir));
+                    TransferOrAdd(newSquares, currentTile);   
+                }
+            }
+            return newSquares;
+        }
+
+        void TransferOrAdd(Squares newSquares, Vector2Int currentTile)
+        {
+            TapeMeasureSquare newSquare;
+            if (squares.ContainsKey(currentTile))
+            {
+                newSquare = squares[currentTile];
+                squares.Remove(currentTile);
+            }
+            else
+            {
+                newSquare = GameObject.Instantiate(SquarePrefab).GetComponent<TapeMeasureSquare>();
+            }
+            newSquare.setPosition(currentTile.x, currentTile.y);
+            newSquares.Add(currentTile, newSquare);
         }
     }
 }
